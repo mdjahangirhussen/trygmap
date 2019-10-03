3430e7cfd74cc0c1b58992149ae4d4d07f84ae0a
mdjahangirhussen <47791938+mdjahangirhussen@users.noreply.github.com>
2019-10-04 00:14:40 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,13 @@
+ var map = new GMaps({
+            el: '#map',
+            lat: 24.886436,
+            lng: 91.880722
+        });
+        map.addMarker({
+            lat: 24.886436,
+            lng: 91.880722,
+            title: 'Sylhet',
+            icon: 'images/map-2.png',
+            animation: google.maps.Animation.BOUNCE
+
+        });
